c6c91c57aa37579cdcaec26670c9f546283ef5b4
ZhehaoLi9705 <511267512@qq.com>
2020-08-31 17:03:17 +0800

    Modified AnnualReturn Func

--- StatisticFunc.py
+++ StatisticFunc.py
@@ -6,79 +6,27 @@ import matplotlib.pyplot as plt
 ###############################################
 # 基本统计指标
 ###############################################
-# def AnnualReturns(NetValueDF):
-#     # 0. 创建备用表
-#     tmpDF = NetValueDF.reset_index()
-
-#     # 1. 计算年份并创建新表
-#     yearIdx = np.unique(tmpDF['日期'].apply(lambda x:str(x)[:4]))                # 筛选出所有年份
-#     yearNetValue = pd.DataFrame(index = yearIdx, columns=NetValueDF.columns)    # 创建新的记录表
-
-#     # 2. 计算每年末资产净值
-#     for y in yearIdx:                                 
-#         mask = tmpDF['日期'].apply(lambda x:str(x)[:4]) == str(y)
-#         end  = tmpDF['日期'][mask].max()                                         # 提取年末日期
-#         yearNetValue.loc[y, :] = NetValueDF.loc[end, :].values
-    
-#     # 3. 计算年间收益率
-#     handleNA = NetValueDF.dropna().iloc[0, :]
-#     yearReturns = yearNetValue / yearNetValue.shift(1, axis=0).fillna(handleNA) - 1.0
-    
-#     return yearReturns
-
-
-# def MonthlyReturns(NetValueDF):
-#     # 0. 创建备用表
-#     tmpDF = NetValueDF.reset_index()
-
-#     # 1. 计算年份并创建新表
-#     monthlyIdx = np.unique(tmpDF['日期'].apply(lambda x:str(x)[:7]))             # 筛选出所有年月
-#     monthlyNetValue = pd.DataFrame(index = monthlyIdx, columns=NetValueDF.columns) # 创建新的记录表
-
-#     # 2. 计算每年末资产净值
-#     for m in monthlyIdx:                                 
-#         mask = tmpDF['日期'].apply(lambda x:str(x)[:7]) == str(m)                
-#         end  = tmpDF['日期'][mask].max()                                         # 提取月末日期
-#         monthlyNetValue.loc[m, :] = NetValueDF.loc[end, :].values
-    
-#     # 3. 计算年间收益率
-#     monthlyReturns = monthlyNetValue / monthlyNetValue.shift(1, axis=0) - 1.0
-    
-#     return monthlyReturns
-
-
-
-
-def PeriodReturns(tradeDF):
-
-    # 计算调仓日
-    trading_day = tradeDF.index[tradeDF['仓位调整'] == 1.0]
-    
-    # 投资组合周期初净值
-    valueDF_prev = tradeDF.loc[trading_day, '投资组合净值'].shift(1, axis=0)
-    
-    # 投资组合周期末净值
-    valueDF_now = tradeDF.shift(1, axis=0).loc[trading_day, '投资组合净值']
-    
-    # 投资组合净值变化
-    valueDF_delta = valueDF_now / valueDF_prev
-    
-    return valueDF_delta 
-
-
-def AnnualReturns(tradeDF):
-
-    # 一个交易周期内各资产的损益
-    valueDF_delta = PeriodReturns(tradeDF)
-    
-    # 一年度内各资产的损益
-    valueDF_annual = valueDF_delta.groupby(valueDF_delta.index.year).prod() - 1.0
-    
-    return valueDF_annual    
-
 
 def DailyReturns(tradeDF):
     return tradeDF['投资组合净值'] / tradeDF['投资组合净值'].shift(1, axis=0) - 1.0
+
+def AnnualReturns(tradeDF):
+    # 0. 创建备用表
+    DailyNV = tradeDF['投资组合净值']
+
+    # 1. 计算年份并创建新表
+    yearIdx = np.unique(tradeDF.index.year)
+    AnnualNV = pd.DataFrame(index=yearIdx, columns=['年末净值'])    # 创建新的记录表
+
+    # 2. 计算每年末资产净值
+    for y in yearIdx:                                 
+        mask = DailyNV.index.year == y
+        AnnualNV.loc[y, '年末净值'] = DailyNV[mask][-1]
+    
+    # 3. 计算年间收益率
+    initialNV = DailyNV.dropna()[0]                                # 开仓第一天净值
+    AnnualReturns = AnnualNV / AnnualNV.shift(1, axis=0).fillna(initialNV) - 1.0
+    return AnnualReturns
 
 def AnnualVolatility(tradeDF):
     return DailyReturns(tradeDF).groupby(tradeDF.index.year).std() * np.sqrt(250)
